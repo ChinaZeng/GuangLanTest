ed2b6daa27142900a4d4841abc1970609238ac9f
ChinaZeng <z.z.w941124@qq.com>
2019-06-01 11:38:52 +0800

    4->1

--- app/src/main/java/com/zzw/guanglan/ui/qianxin/QianXinListActivity.java
+++ app/src/main/java/com/zzw/guanglan/ui/qianxin/QianXinListActivity.java
@@ -445,8 +445,8 @@ public class QianXinListActivity extends BaseActivity implements
             ToastUtils.showToast("请先定位");
             return;
         }
-        if (!compareDistance(bean, 4)) {
-            ToastUtils.showToast("当前位置和该光缆的位置大于4公里，请确认定位位置!");
+        if (!compareDistance(bean, 1)) {
+            ToastUtils.showToast("当前位置和该光缆的位置大于1公里，请确认定位位置!");
             return;
         }
 
